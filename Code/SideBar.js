bc80eb65472ebb64dd3e25b7da001a7324468756
Supreme3Bye5423 <116857989+Supreme3Bye5423@users.noreply.github.com>
2022-11-20 23:01:27 +0700

    Add files via upload

---
+++ Code/SideBar.js
@@ -0,0 +1,17 @@
+var mini = true;
+
+function toggleSidebar() {
+  if (mini) {
+    console.log("opening sidebar");
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.backgroundColor = "black";
+    this.mini = false;
+  } else {
+    console.log("closing sidebar");
+    document.getElementById("mySidebar").style.width = "85px";
+    document.getElementById("main").style.marginLeft = "85px";
+    document.getElementById("mySidebar").style.backgroundColor = "white";
+    this.mini = true;
+  }
+}
